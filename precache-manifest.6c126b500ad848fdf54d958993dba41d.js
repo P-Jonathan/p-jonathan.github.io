485f7201b78a50808598467ebab84ba606836546
John <JonathanNicolasPaz@Outlook.com>
2020-02-03 22:39:27 -0300

    Updates

---
+++ precache-manifest.6c126b500ad848fdf54d958993dba41d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1897b2afd06ab6ab4d3ceb48f21b14a6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d38921be68b439417933",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "25084ea9319f1b05744f",
+    "url": "/static/js/2.2c579004.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.2c579004.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d38921be68b439417933",
+    "url": "/static/js/main.4a60e30a.chunk.js"
+  },
+  {
+    "revision": "10b5630a3fd6a477ed3a",
+    "url": "/static/js/runtime-main.67911446.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
